59ad3b0df0156ced561e59084b752e2b8fa34840
robmckinnon <rob@movingflow>
2013-06-04 17:07:44 +0100

    refactoring

--- spec/lib/heroku_gandi_dns/zone_version_spec.rb
+++ spec/lib/heroku_gandi_dns/zone_version_spec.rb
@@ -3,53 +3,51 @@ require_relative '../../../lib/heroku_gandi_dns/zone_version'
 
 describe HerokuGandiDns::ZoneVersion do
 
-  describe 'constructed with session and domain' do
+  let(:version_id) { mock }
+  let(:ip_address) { mock }
 
-    let(:version_id) { mock }
-    let(:ip_address) { mock }
+  before do
+    @version = HerokuGandiDns::ZoneVersion.new(version_id, records)
+  end
 
-    before do
-      @version = HerokuGandiDns::ZoneVersion.new(version_id, records)
+  describe 'after initialized' do
+    let(:records)    { mock }
+
+    it 'should have version_id' do
+      @version.version_id.must_equal version_id
     end
 
-    describe 'after initialized' do
-      let(:records)    { mock }
-
-      it 'should have version_id' do
-        @version.version_id.must_equal version_id
-      end
-
-      it 'should have records' do
-        @version.records.must_equal records
-      end
-    end
-
-    describe 'with no A records' do
-      let(:records)    { [mock(type: 'M')] }
-
-      it 'should have single_a_record? false' do
-        @version.single_a_record?.must_equal false
-      end
-    end
-
-    describe 'with two A records' do
-      let(:records)    { [mock(type: 'A'), mock(type: 'A')] }
-
-      it 'should have single_a_record? false' do
-        @version.single_a_record?.must_equal false
-      end
-    end
-
-    describe 'with one A record' do
-      let(:records)    { [stub(type: 'A', value: ip_address)] }
-
-      it 'should have single_a_record? true' do
-        @version.single_a_record?.must_equal true
-      end
-
-      it 'should have ip_address correct' do
-        @version.ip_address.must_equal ip_address
-      end
+    it 'should have records' do
+      @version.records.must_equal records
     end
   end
+
+  describe 'with no A records' do
+    let(:records)    { [mock(type: 'M')] }
+
+    it 'should have single_a_record? false' do
+      @version.single_a_record?.must_equal false
+    end
+  end
+
+  describe 'with two A records' do
+    let(:records)    { [mock(type: 'A'), mock(type: 'A')] }
+
+    it 'should have single_a_record? false' do
+      @version.single_a_record?.must_equal false
+    end
+  end
+
+  describe 'with one A record' do
+    let(:records)    { [stub(type: 'A', value: ip_address)] }
+
+    it 'should have single_a_record? true' do
+      @version.single_a_record?.must_equal true
+    end
+
+    it 'should have ip_address correct' do
+      @version.ip_address.must_equal ip_address
+    end
+  end
+
 end
